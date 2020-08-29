5e27aef110d2a1530d576038a2b38c407daeb5a4
JadenisJelu <jelu022498@gmail.com>
2020-08-29 17:21:10 +0800

    Fixed username not found case and added HB

--- server.py
+++ server.py
@@ -20,6 +20,19 @@ def make_comms(msg):
             reply = msg + ' is the starter!'
     return reply
 
+def reply_happy_birthday():
+    reply = '''Happy Birthday to You
+Happy Birthday to You
+Happy Birthday to You
+Happy Birthday to You
+
+恭祝你福壽與天齊
+慶賀你生辰快樂
+年年都有今日 
+歲歲都有今朝
+恭喜你 恭喜你
+'''
+    return reply
 update_id = None
 
 while True:
@@ -33,10 +46,21 @@ while True:
             except:
                 message = None
             from_ = item["message"]["chat"]["id"]
-            from_username = item["message"]["from"]["username"]
+
+            # check username exists or not
+            if 'username' in item["message"]["from"].keys():
+                from_username = item["message"]["from"]["username"]
+            else:
+                from_username = 'sender'
+                
             # check if bot is added to group
             if 'new_chat_participant' in item["message"].keys():
                 pass
+
+            # Happy Birthday
+            if 'happy birthday' in message.lower():
+                bot.send_message(reply_happy_birthday(), from_)                
+               
             reply = make_reply(message, from_username)
             bot.send_message(reply, from_)
             if reply != None:
